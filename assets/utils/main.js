8eebd5479abe5a35487c3cd347f9e2a86c735c11
irvingzhao <irvinezhao@webank.com>
2018-10-18 18:09:52 +0800

    changhes

--- assets/utils/main.js
+++ assets/utils/main.js
@@ -3,16 +3,19 @@ jQuery(document)
         $('#myCarousel')
             .on('slid.bs.carousel', function () {
                 if ($('.item.active').hasClass('car-item-1')) {
-                    $('.carousel-desc_title').text('1111111111');
-                    $('.carousel-desc_content').text('1111111111');
+                    $('.carousel-desc_title').text('Supply Chain Finance');
+                    $('.carousel-desc_content').text('Promoting partnerships and collaborations among entities, private or public usi' +
+                            'ng the FISCO BCOS platform.');
                 }
                 if ($('.item.active').hasClass('car-item-2')) {
-                    $('.carousel-desc_title').text('22222222');
-                    $('.carousel-desc_content').text('22222222');
+                    $('.carousel-desc_title').text('Points alliance');
+                    $('.carousel-desc_content').text('An alliance is a relationship among people, groups, or states that have joined t' +
+                            'ogether for mutual benefit');
                 }
                 if ($('.item.active').hasClass('car-item-3')) {
-                    $('.carousel-desc_title').text('33333333');
-                    $('.carousel-desc_content').text('33333333');
+                    $('.carousel-desc_title').text('Judicial services');
+                    $('.carousel-desc_content').text('Guangzhou Arbitration Commission, WeBank, and Hangzhou Yibi' +
+                            ' the "arbitration blockchain"');
                 }
 
             })
