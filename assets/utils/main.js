ebfbc57bc2ce183c20b5470a927f8485cc224f97
irvingzhao <irvinezhao@webank.com>
2018-10-23 10:45:16 +0800

    v_1

--- assets/utils/main.js
+++ assets/utils/main.js
@@ -4,8 +4,8 @@ jQuery(document)
             .on('slid.bs.carousel', function () {
                 if ($('.item.active').hasClass('car-item-1')) {
                     $('.carousel-desc_title').text('Supply Chain Finance');
-                    $('.carousel-desc_content').text('Promoting partnerships and collaborations among entities, private or public usi' +
-                            'ng the FISCO BCOS platform.');
+                    $('.carousel-desc_content').text('Promoting partnerships and collaborations among entities, private or public usin' +
+                            'g the FISCO BCOS platform.');
                 }
                 if ($('.item.active').hasClass('car-item-2')) {
                     $('.carousel-desc_title').text('Points alliance');
@@ -14,8 +14,8 @@ jQuery(document)
                 }
                 if ($('.item.active').hasClass('car-item-3')) {
                     $('.carousel-desc_title').text('Judicial services');
-                    $('.carousel-desc_content').text('Guangzhou Arbitration Commission, WeBank, and Hangzhou Yibi' +
-                            ' the "arbitration blockchain"');
+                    $('.carousel-desc_content').text('Guangzhou Arbitration Commission, WeBank, and Hangzhou Yibi the "arbitration blo' +
+                            'ckchain"');
                 }
 
             })
